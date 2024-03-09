fc2572c5091710953cdbf8130e3c6eb71e2bc17c
Galaxy <108278887+IIIevanIII00@users.noreply.github.com>
2024-03-09 16:15:32 +0100

    Add files via upload

---
+++ Galaxy_Auto_Top_V3.lua
@@ -0,0 +1,47 @@
+--[[
+
+    -------------------------------[Moveset for each damage]-------------------------------
+
+For melee moves i suggest use:
+
+    Move1 = "Flash Strike" 
+    Move2 = "Sweep Kick"
+
+Or this one:
+  
+   Move1 = "Deadly Dance" 
+   Move2 = "Anger Rush"
+   Move3 = "Meteor Crash"
+
+And for ki moves i suggest use:
+
+  Move1 = "Blaster Meteor"
+  Move2 = "Chain Destructo Disk"
+
+And for big melee moveset i suggest this one:
+ 
+  Move1 = "Neo Wolf Fang Fist"
+  Move2 = "Wolf Fang Fist"
+  Move3 = "Anger Rush"
+  Move4 =  "Flash Skewer"
+  Move5 =  "Meteor Crash"
+  Move6 =  "Deadly Dance"
+  Move7 = "TS Molotov"
+  Move8 = "Sweep Kick"
+  Move9 = "God Slicer"
+]]--
+
+Move1 = "Chain Destructo Disk";
+Move2 = "Blaster Meteor";
+Move3 = "Anger Rush";
+
+_G.HardTop = false
+_G.AutoWish = true
+_G.Wish = "Zenni" -- Zenni, Exp, Prestige, Levelcap
+_G.RejoinTime = 200
+_G.Invis = false
+_G.Ki = false -- you need a LOT of ki max
+
+---------------------------------------------------------------------------------
+
+loadstring(game:HttpGet("https://github.com/IIIevanIII00/Galaxy-Auto-Top/blob/main/Galaxy%20Auto%20Top%20V3.lua"))()
